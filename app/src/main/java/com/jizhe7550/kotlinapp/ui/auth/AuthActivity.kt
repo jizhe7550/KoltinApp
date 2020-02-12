316425b16b995f949372a09a111ab1abe9064edf
吉喆 <jizhe@chuangyejia.com>
2020-02-12 20:17:24 +1300

    finish some fragment with Navigation components

--- app/src/main/java/com/jizhe7550/kotlinapp/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/jizhe7550/kotlinapp/ui/auth/AuthActivity.kt
@@ -1,7 +1,8 @@
-package com.jizhe7550.kotlinapp
+package com.jizhe7550.kotlinapp.ui.auth
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.jizhe7550.kotlinapp.R
 
 class AuthActivity : AppCompatActivity(){
 
