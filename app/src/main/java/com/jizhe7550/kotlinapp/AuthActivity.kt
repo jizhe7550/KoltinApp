61d87aba1ec82c2b01bd1e49699fb96c8363e8c9
吉喆 <jizhe@chuangyejia.com>
2020-02-12 19:19:03 +1300

    upload initial project including lots of res

---
+++ app/src/main/java/com/jizhe7550/kotlinapp/AuthActivity.kt
@@ -0,0 +1,13 @@
+package com.jizhe7550.kotlinapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AuthActivity : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
